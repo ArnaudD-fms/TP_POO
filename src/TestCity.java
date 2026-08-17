7ab0f94efa91800adc90c720abc513e4f344cd66
Arnaud Delperier <arnaud.delperier@fms-ea.com>
2026-08-17 11:37:18 +0200

    Comptage du nombre d'instances de City

--- src/TestCity.java
+++ src/TestCity.java
@@ -1,8 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TestCity {
     public static void main(String[] args) {
         City toulouse = new City("Toulouse", "France", 514819);
         City bordeaux = new City("Bordeaux", 267991);
         City tokyo = new City("Tokyo", "Japon", 14000000);
+
+        List<Object> cities = new ArrayList<>(
+                Arrays.asList(toulouse, bordeaux, tokyo)
+        );
+
 
         System.out.println(toulouse);
         System.out.println(bordeaux);
@@ -14,5 +24,13 @@ public class TestCity {
         bordeaux.display();
         bordeaux.setCountry("France");
         bordeaux.display();
+
+        int nbCities = 0;
+        for (Object city : cities) {
+            if (city instanceof City) {
+                nbCities++;
+            }
+        }
+        System.out.println("Il y a " + nbCities + " instances de villes.");
     }
 }
