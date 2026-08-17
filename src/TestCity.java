0026d1fd5d1e6077af30cb3674a5a0751746758e
Arnaud Delperier <arnaud.delperier@fms-ea.com>
2026-08-17 10:57:13 +0200

    Ajout de la classe City et de ses attributs de bases

---
+++ src/TestCity.java
@@ -0,0 +1,14 @@
+public class TestCity {
+    public static void main(String[] args) {
+        City toulouse = new City("Toulouse", "France", 514819);
+        City bordeaux = new City("Bordeaux", "France", 267991);
+        City tokyo = new City("Tokyo", "Japon", 14000000);
+
+        System.out.println(toulouse);
+        System.out.println(bordeaux);
+        System.out.println(tokyo);
+
+        toulouse.setPopulation(toulouse.getPopulation() + 20000);
+        System.out.println(toulouse);
+    }
+}
